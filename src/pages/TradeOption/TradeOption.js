6aa0c5ab72327f5cbc16378c1fe76f888b872259
이지혜 <jihye@ijihyeui-MacBookAir.local>
2023-08-27 23:59:09 +0900

    Modify : ui 완성

--- src/pages/TradeOption/TradeOption.js
+++ src/pages/TradeOption/TradeOption.js
@@ -1,10 +1,13 @@
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import HeaderTop from '../../components/HeaderTop/HeaderTop';
 import HeaderMain from '../../components/HeaderMain/HeaderMain';
+import BidDeadline from '../../components/BidDeadline/BidDeadline';
 import './TradeOption.scss';
 
 const TradeOption = () => {
   const navigate = useNavigate();
+  const [isToggled, setIsToggled] = useState(true);
 
   return (
     <div className="tradeOption">
@@ -45,24 +48,53 @@ const TradeOption = () => {
                 <div className="instantGroup">
                   <div className="tabArea">
                     <ul className="tabList">
-                      <li className="item">
+                      <li
+                        onClick={() => {
+                          setIsToggled(false);
+                        }}
+                        className={!isToggled ? ' on item' : 'item'}
+                      >
                         <button className="itemLink">구매 입찰</button>
                       </li>
-                      <li className="item on">
+
+                      <li
+                        onClick={() => {
+                          setIsToggled(true);
+                        }}
+                        className={isToggled ? 'on item' : 'item'}
+                      >
                         <button className="itemLink ">즉시 구매</button>
                       </li>
                     </ul>
                   </div>
-                  <div className="priceNow">
-                    <dl className="priceNowBox">
-                      <dt className="priceNowTitle">즉시 구매가</dt>
-                      <dd className="price">
-                        <span className="amount">100,000</span>
-                        <span className="unit">원</span>
-                      </dd>
-                    </dl>
-                    <div className="priceWarning" />
-                  </div>
+
+                  {isToggled ? (
+                    <div className="priceNow">
+                      <dl className="priceNowBox">
+                        <dt className="priceNowTitle">즉시 구매가</dt>
+                        <dd className="price">
+                          <span className="amount">100,000</span>
+                          <span className="unit">원</span>
+                        </dd>
+                      </dl>
+                      <div className="priceWarning" />
+                    </div>
+                  ) : (
+                    <div className="priceNow">
+                      <dl className="priceNowBox">
+                        <dt className="priceNowTitle">구매 희망가</dt>
+                        <dd className="price">
+                          <input
+                            className="inputAmount"
+                            type="number"
+                            placeholder="희망가 입력"
+                          />
+                          <span className="unit">원</span>
+                        </dd>
+                      </dl>
+                      <div className="priceWarning" />
+                    </div>
+                  )}
                   <div className="priceBlind">
                     <p className="priceBlindEmpty">
                       총 결제금액은 다음 화면에서 계산됩니다.
@@ -70,6 +102,7 @@ const TradeOption = () => {
                   </div>
                 </div>
               </div>
+              {isToggled ? '' : <BidDeadline />}
               <div className="totalConfirm">
                 <div className="priceTotal">
                   <dl className="priceBox">
@@ -78,16 +111,30 @@ const TradeOption = () => {
                   </dl>
                   <span className="priceWarning" />
                 </div>
-                <div className="btnConfirm">
-                  <button
-                    className="nextBtn"
-                    onClick={() => {
-                      navigate('/payment');
-                    }}
-                  >
-                    즉시 구매 계속
-                  </button>
-                </div>
+                {isToggled ? (
+                  <div className="btnConfirm">
+                    <button
+                      className="nextBtn"
+                      onClick={() => {
+                        navigate('/payment');
+                      }}
+                    >
+                      즉시 구매 계속
+                    </button>
+                  </div>
+                ) : (
+                  <div className="btnConfirm">
+                    <button
+                      className="nextBtn disabled"
+                      onClick={() => {
+                        navigate('/payment');
+                      }}
+                      disabled
+                    >
+                      구매 입찰 계속
+                    </button>
+                  </div>
+                )}
               </div>
             </div>
           </div>
